0988c69b272d765f2676a51b387aa001a2e60fcb
otodavid <dieavid01@gmail.com>
2025-08-04 14:56:06 -0500

    implemented main app flow

--- BankingApp/Program.cs
+++ BankingApp/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using BankingApp.Models;
 
 namespace BankingApp;
 
@@ -6,6 +6,91 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World");
+        Console.WriteLine("Enter your name: ");
+        string? name = Console.ReadLine();
+
+        if (string.IsNullOrEmpty(name))
+        {
+            Console.WriteLine("A valid name is needed to create an account");
+            return;
+        }
+
+        var account = new BankAccount(name);
+
+        while (true)
+        {
+            DisplayMenu();
+
+            var choice = Console.ReadLine();
+            Console.Clear();
+
+            switch (choice)
+            {
+                case "1":
+                    Console.WriteLine("Enter amount to deposit: ");
+                    ProcessTransaction(account, TransactionType.Deposit);
+                    break;
+
+                case "2":
+                    Console.WriteLine("Enter amount to withdraw: ");
+                    ProcessTransaction(account, TransactionType.Withdraw);
+                    break;
+
+                case "3":
+                    Console.WriteLine(account.GetBalance());
+                    break;
+
+                case "4":
+                    Console.WriteLine("Thank you for banking with us");
+                    return;
+
+                default:
+                    Console.WriteLine("Invalid Option");
+                    break;
+            }
+        }
+
     }
+
+    private static void DisplayMenu()
+    {
+        Console.WriteLine("\nChoose an action:");
+        Console.WriteLine("1. Deposit:");
+        Console.WriteLine("2. Withdraw:");
+        Console.WriteLine("3. View Balance:");
+        Console.WriteLine("4. Exit:");
+        Console.Write("Enter your choice: ");
+    }
+
+    private static void ProcessTransaction(BankAccount account, TransactionType transactionType)
+    {
+        if (decimal.TryParse(Console.ReadLine(), out var amount))
+        {
+            try
+            {
+                if (transactionType == TransactionType.Deposit)
+                {
+                    account.Deposit(amount);
+                }
+                else
+                {
+                    account.Withdraw(amount);
+                }
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid amount. Please enter a positive number");
+        }
+    }
+}
+
+public enum TransactionType
+{
+    Deposit,
+    Withdraw
 }
