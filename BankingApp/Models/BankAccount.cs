d5f779e49f16e164e7d256f0844ee95b84e62ac1
otodavid <dieavid01@gmail.com>
2025-08-04 14:55:31 -0500

    finished bank account class

---
+++ BankingApp/Models/BankAccount.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingApp.Models
+{
+    public class BankAccount(string owner)
+    {
+        public string Owner { get; set; } = owner;
+        public decimal Balance { get; set; } = 0;
+
+        public void Deposit(decimal amount)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentException("Deposit must be greater than 0");
+            }
+
+            Balance += amount;
+            Console.WriteLine($"{amount:C} deposited successfully");
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (amount < 0) throw new ArgumentException("Withdraw amount must be positive");
+            if (amount > Balance)
+            {
+                throw new InvalidOperationException("Insufficient Funds");
+            }
+
+            Balance -= amount;
+
+            Console.WriteLine($"{amount:C} withdrawn successfully");
+
+        }
+
+        public string GetBalance()
+        {
+            return $"Balance: {Balance:C}";
+        }
+    }
+}
