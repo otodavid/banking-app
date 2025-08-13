104591919b1e9126e06ebd16e43832b70a258a7f
otodavid <dieavid01@gmail.com>
2025-08-13 11:00:51 -0500

    created Transaction class

---
+++ BankingApp/Models/Transaction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingApp.Models
+{
+    public class Transaction
+    {
+        public DateTime Date { get; set; }
+        public decimal Amount { get; set; }
+        public decimal AmountAfter { get; set; }
+        public TransactionType Type { get; set; }
+    }
+}
